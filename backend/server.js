3df021d733b5914ccc1e454c72809b976344fbab
AlfiyaSiddique <alfiyasiddique1708@gmail.com>
2024-12-28 17:06:08 +0530

    email completion

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,12 @@ dotenv.config();
 const app = express();
 
 app.use(express.json())
-// app.use(
-//   cors({
-//     origin: process.env.FRONTENDURL,
-//   })
-// );
-app.use(cors())
+app.use(
+  cors({
+    origin: process.env.FRONTENDURL,
+  })
+);
+// app.use(cors())
 
 app.get("/", (req, res) => {
   res.send("Welcome to Arasco India Private Limited!");
